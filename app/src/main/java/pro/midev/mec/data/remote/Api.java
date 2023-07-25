cc150202ae3078ecc52623cf73255e113aec0da4
elka_vt <elka-vt@yandex.ru>
2023-07-25 19:31:50 +0300

    add files for architecture

---
+++ app/src/main/java/pro/midev/mec/data/remote/Api.java
@@ -0,0 +1,4 @@
+package pro.midev.mec.data.remote;
+
+public interface Api {
+}
---
+++ app/src/main/java/pro/midev/mec/presentation/di/ViewModelModule.kt
@@ -0,0 +1,5 @@
+package pro.midev.mec.presentation.di
+
+import org.koin.dsl.module
+
+val viewModelModule = module {}
