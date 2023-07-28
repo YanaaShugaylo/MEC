2398a3265883090ffe0f8935ec1692129cfc029e
semenmyslin <110462243+semenmyslin@users.noreply.github.com>
2023-07-28 10:08:20 +0300

    todo

--- app/src/main/java/pro/midev/mec/presentation/ui/components/TextField.kt
+++ app/src/main/java/pro/midev/mec/presentation/ui/components/TextField.kt
@@ -23,6 +23,7 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
@@ -83,6 +84,7 @@ fun TextFieldInput(
             }
 
             Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(
                     bottom = if (label == null) 18.dp else 11.dp,
                     top = if (label == null) 18.dp else 0.dp
