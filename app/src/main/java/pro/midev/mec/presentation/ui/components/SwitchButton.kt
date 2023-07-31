0301d37e905df9dae9e8c973111b3cc371741112
YanaaShugaylo <109665860+YanaaShugaylo@users.noreply.github.com>
2023-07-31 10:56:19 +0300

    Завершение верстки switch button

--- app/src/main/java/pro/midev/mec/presentation/ui/components/SwitchButton.kt
+++ app/src/main/java/pro/midev/mec/presentation/ui/components/SwitchButton.kt
@@ -1,4 +1,4 @@
-package pro.midev.mec.presentation.ui.test
+package pro.midev.mec.presentation.ui.components
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Spring
