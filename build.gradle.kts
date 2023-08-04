3e14a47f477ce6eeabfa057b1e0375b08d57c554
YanaaShugaylo <109665860+YanaaShugaylo@users.noreply.github.com>
2023-08-04 09:41:13 +0300

    Features radiobutton (#2)
    * Добавление radio button
    
    * Добавление radio button
    
    * Завершение верстки radio button
    
    * Небольшие правки после ПР-а

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,17 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    val kotlin_version by extra("1.7.20")
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+    repositories {
+        mavenCentral()
     }
 }
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
